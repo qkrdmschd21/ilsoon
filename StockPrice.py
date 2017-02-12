20341686f8203a43bc0c7d7a1c279af306cad471
ilsooon <klklnnnnd@gmail.com>
2017-02-12 16:46:07 +0900

    NEw

--- StockPrice.py
+++ StockPrice.py
@@ -1,2 +1,13 @@
 import pandas as pd
-import pandas_datareader.data as make diff
+import pandas_datareader.data as web
+import datetime
+import sqlite3
+
+def makedatetime(starttime,endtime):
+    start = datetime.datetime(starttime[0],starttime[1],starttime[2])
+    end = datetime.datetime(endtime[0],endtime[1],endtime[2])
+    print(start)
+
+if __name__ == '__main__':
+    starttime = [2010, 1, 1]
+    endtime = [2011, 1, 1]
